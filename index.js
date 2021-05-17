b8199425123c899bec701e4163ac20a9bf9e6ee2
serhii <telp.s.cheremisin@gmail.com>
2021-05-17 19:18:54 +0300

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
+
+dotenv.config();
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const {
+    chat: { id },
+  } = msg;
+
+  if (msg.from.first_name.toLowerCase() !== "fox") {
+    let random = Math.floor(Math.random() * 100);
+    if (random <= 30) {
+      bot.sendMessage(id, msg.from.first_name + " krab");
+    } 
+  } else {
+    bot.sendMessage(id, msg.from.first_name + " krab");
+  }
+});
