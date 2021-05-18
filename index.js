b22a2b694e5dfe912907c7a0177dba4cd524d0c9
Cheremisin, Serhii <scheremisin@hq.telesens.lan>
2021-05-18 09:30:15 +0300

    Added animals and possibility 50%

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-import dotenv from 'dotenv';
-import TelegramBot from 'node-telegram-bot-api';
+import dotenv from "dotenv";
+import TelegramBot from "node-telegram-bot-api";
 
 dotenv.config();
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
+
+const animals = ["krab", "salmon", "shell", "seal", "octopus"];
+
+const possibility = (percent) => Math.floor(Math.random() * 100) <= percent;
 
 bot.on("message", (msg) => {
   const {
     chat: { id },
   } = msg;
 
-  if (msg.from.first_name.toLowerCase() !== "eugene") {
-    let random = Math.floor(Math.random() * 100);
-    if (random <= 30) {
-      bot.sendMessage(id, msg.from.first_name + " krab");
-    } 
-  } else {
-    bot.sendMessage(id, msg.from.first_name + " krab");
+  const randomIndexWithoutKrab = Math.floor(1 + Math.random() * 4);
+  const isNameEugene = msg.from.first_name.toLowerCase() === "eugene";
+  const callMessage = (animal) => {
+    bot.sendMessage(id, msg.from.first_name + " " + animal);
+  };
+
+  if (!isNameEugene && possibility(30)) {
+    callMessage(possibility(50) ? animals[0] : animals[randomIndexWithoutKrab]);
+  }
+  if (isNameEugene) {
+    callMessage(animals[0]);
   }
 });
